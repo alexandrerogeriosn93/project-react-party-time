1fa654508785301ab6444b5d6045dfac2cef4dd3
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-25 08:41:09 -0300

    feat: create Delete request to delete Service by id

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -35,9 +35,30 @@ const serviceController = {
 
       if (!service) {
         res.status(404).json({ msg: "Serviço não encontrado!" });
+        return;
       }
 
       res.json(service);
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  },
+
+  delete: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const service = await ServiceModel.findById(id);
+
+      if (!service) {
+        res.status(404).json({ msg: "Serviço não encontrado!" });
+        return;
+      }
+
+      const deletedService = await ServiceModel.findByIdAndDelete(id);
+
+      res
+        .status(200)
+        .json({ deletedService, msg: "Serviço deletado com sucesso" });
     } catch (error) {
       console.log(`Error: ${error}`);
     }
